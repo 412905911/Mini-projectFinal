f8da1893a9cc866db355eecd789984106e04843a
412905911 <60133192+412905911@users.noreply.github.com>
2020-05-23 13:46:10 +0100

    Update login.php

--- html/login.php
+++ html/login.php
@@ -1,8 +1,21 @@
 <?php
     session_start();
     include("conn.php");
-    $email = $_POST['email'];
-    $password = $_POST['userpass'];
-    $result = mysqli_query($conn, "SELECT IFNULL((SELECT 'Y' from user where Email = '".$email."' AND Password = '".$password."' limit 1),'N')");
-    echo $result;
+    
+    $sql="select * from user where email='$_POST[email]'and password='$_POST[password]'";
+    $result=mysqli_query($conn,$sql);
+    $row=mysqli_fetch_assoc($result);
+
+    if ($row==true) {
+        $url = "addPost.html?email=".$_POST['email'];
+        echo "<script type='text/javascript'>";
+        echo "window.location.href='$url'";
+        echo "</script>"; 
+    }else{
+        $url = "login.html";
+        echo "<script type='text/javascript'>";
+        echo "window.location.href='$url'";
+        echo "</script>"; 
+    }
+    
 ?>
